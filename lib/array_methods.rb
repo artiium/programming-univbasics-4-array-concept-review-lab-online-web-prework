3b207437fffdcc73869e50391796952d5d176929
Artiium Arbyummi <artiium21@gmail.com>
2019-09-05 01:58:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,18 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def find_element_index(array, value)
+  index = 0 
+  while index < array.size do
+    if array[index] == value
+      return index
+    end
+    index += 1
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min 
 end
