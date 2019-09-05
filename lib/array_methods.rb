2aad3c1bd3ced725e3ce761ae2504cd9ae54f367
Artiium Arbyummi <artiium21@gmail.com>
2019-09-05 01:41:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,10 @@
 def find_element_index(array, value_to_find)
   counter = 0
-  while 
-
+  while counter < array.size do
+    if array[counter] == value_to_find
+      return counter
+    end
+  end
 end
 
 def find_max_value(array)
